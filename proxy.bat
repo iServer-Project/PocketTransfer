14f024ef89360a5d63ec8bd94c660a19aaaed8de
TheLucyClub Github Team <thelucyclub@gmail.com>
2016-04-18 19:58:56 -0400

    Update proxy.bat

--- proxy.bat
+++ proxy.bat
@@ -1,5 +1,15 @@
-echo [KiNET] Initializing process...
-echo bin\load.sh exit 277
-goto :a
-:a
-goto :a
+cd ..
+  if exist KiNET.jfs (
+  echo [KiNET] Initializing process...
+  echo bin\load.sh exit 277
+  goto :a
+  :a
+  goto :a
+) else (
+  echo [ERROR] The KiNET addon is required to run!
+  goto :b
+  :b
+  echo.
+  ping e.4.g.2:2234 -n 1>nul
+  goto :b
+)
